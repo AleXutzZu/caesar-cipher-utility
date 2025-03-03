a6eab11c19fd449b2cacfb49a384411a3c97ebd5
AleXutzZu <davidalexrobertyt@gmail.com>
2025-03-03 12:02:22 +0200

    Added signature of read_distribution function

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+#define ALPHABET_SIZE 26
+
+/**
+ * Reads the distribution of letters in the English alphabet from the specified filename. The file should
+ * contain a line with a floating point number for each of the letters representing its frequency
+ * @param filename The filename
+ * @param distribution an array in which the read data is to be stored
+ */
+void read_distribution(const char *filename, double distribution[ALPHABET_SIZE]);
+
 int main(void) {
     printf("Hello, World!\n");
     return 0;
