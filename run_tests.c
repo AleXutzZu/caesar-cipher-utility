7c5307a36419262c179376d8956acf93c62ee39e
AleXutzZu <davidalexrobertyt@gmail.com>
2025-03-09 23:45:23 +0200

    Modified the test runner to display the stats better and included stats for 2nd and 3rd guesses

--- run_tests.c
+++ run_tests.c
@@ -20,7 +20,8 @@ int main(int argc, char **argv) {
     char text[MAX_SIZE + 1];
 
     int total_tests = 0;
-    int passed_chi = 0, passed_cosine = 0, passed_euclidean = 0;
+    int passed_chi[3] = {0}, passed_cosine[3] = {0}, passed_euclidean[3] = {0};
+
     while (fscanf(input, "%d %[^\n]", &actual_shift, text) == 2) {
         total_tests++;
         double top_distances[TOP_N];
@@ -28,20 +29,49 @@ int main(int argc, char **argv) {
 
         break_cipher(text, top_shifts, top_distances, reference_distribution_histogram, chi_squared_distance);
 
-        int reverse_shift = 26 - top_shifts[0];
-        if (reverse_shift == actual_shift) passed_chi++;
+        for (int i = 0; i < TOP_N; ++i) {
+            int reverse_shift = 26 - top_shifts[i];
+            if (reverse_shift == actual_shift) passed_chi[i]++;
+        }
 
         break_cipher(text, top_shifts, top_distances, reference_distribution_histogram, cosine_distance);
-        reverse_shift = 26 - top_shifts[0];
-        if (reverse_shift == actual_shift) passed_cosine++;
+        for (int i = 0; i < TOP_N; ++i) {
+            int reverse_shift = 26 - top_shifts[i];
+            if (reverse_shift == actual_shift) passed_cosine[i]++;
+        }
 
         break_cipher(text, top_shifts, top_distances, reference_distribution_histogram, euclidean_distance);
-        reverse_shift = 26 - top_shifts[0];
-        if (reverse_shift == actual_shift) passed_euclidean++;
+        for (int i = 0; i < TOP_N; ++i) {
+            int reverse_shift = 26 - top_shifts[i];
+            if (reverse_shift == actual_shift) passed_euclidean[i]++;
+        }
     }
     printf("Total tests: %d\n", total_tests);
-    printf("Passed for Chi: %d (%.2lf%%)\n", passed_chi, passed_chi * 100.0 / total_tests);
-    printf("Passed for Cosine: %d (%.2lf%%)\n", passed_cosine, passed_cosine * 100.0 / total_tests);
-    printf("Passed for Euclidean: %d (%.2lf%%)\n", passed_euclidean, passed_euclidean * 100.0 / total_tests);
+    printf("===Chi Stats===\n");
+    double total_percent = 0;
+    for (int i = 0; i < TOP_N; ++i) {
+        double percent = passed_chi[i] * 100.0 / total_tests;
+        total_percent += percent;
+        printf("Passed #%d guess: %d (%.2lf%%)\n", i + 1, passed_chi[i], percent);
+    }
+    printf("Total %%: %2.lf%%\n", total_percent);
+
+    printf("===Cosine Stats===\n");
+    total_percent = 0;
+    for (int i = 0; i < TOP_N; ++i) {
+        double percent = passed_cosine[i] * 100.0 / total_tests;
+        total_percent += percent;
+        printf("Passed #%d guess: %d (%.2lf%%)\n", i + 1, passed_cosine[i], percent);
+    }
+    printf("Total %%: %2.lf%%\n", total_percent);
+
+    printf("===Euclidean Stats===\n");
+    total_percent = 0;
+    for (int i = 0; i < TOP_N; ++i) {
+        double percent = passed_euclidean[i] * 100.0 / total_tests;
+        total_percent += percent;
+        printf("Passed #%d guess: %d (%.2lf%%)\n", i + 1, passed_euclidean[i], percent);
+    }
+    printf("Total %%: %2.lf%%\n", total_percent);
     return 0;
 }
